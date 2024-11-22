2d1bd31f379bf9ee7beaf42a2e69d0524f2dc598
mstf-kara <mstfkaraaa@gmail.com>
2024-11-22 11:07:02 +0300

    Add files via upload

---
+++ ZAMANLAYICI/ZAMANLAYICI/ZAMANLAYICI/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace ZAMANLAYICI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbSaat = new System.Windows.Forms.Label();
+            this.lbDakika = new System.Windows.Forms.Label();
+            this.lbSaniye = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 173);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(154, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dakika";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(283, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Saniye";
+            // 
+            // lbSaat
+            // 
+            this.lbSaat.AutoSize = true;
+            this.lbSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSaat.Location = new System.Drawing.Point(12, 186);
+            this.lbSaat.Name = "lbSaat";
+            this.lbSaat.Size = new System.Drawing.Size(77, 54);
+            this.lbSaat.TabIndex = 3;
+            this.lbSaat.Text = "00";
+            // 
+            // lbDakika
+            // 
+            this.lbDakika.AutoSize = true;
+            this.lbDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDakika.Location = new System.Drawing.Point(138, 186);
+            this.lbDakika.Name = "lbDakika";
+            this.lbDakika.Size = new System.Drawing.Size(77, 54);
+            this.lbDakika.TabIndex = 4;
+            this.lbDakika.Text = "00";
+            // 
+            // lbSaniye
+            // 
+            this.lbSaniye.AutoSize = true;
+            this.lbSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSaniye.Location = new System.Drawing.Point(264, 186);
+            this.lbSaniye.Name = "lbSaniye";
+            this.lbSaniye.Size = new System.Drawing.Size(77, 54);
+            this.lbSaniye.TabIndex = 5;
+            this.lbSaniye.Text = "00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(95, 186);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 54);
+            this.label7.TabIndex = 6;
+            this.label7.Text = ":";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(221, 186);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 54);
+            this.label8.TabIndex = 7;
+            this.label8.Text = ":";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(73, 243);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Başlat";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(201, 243);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Durdur";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 437);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lbSaniye);
+            this.Controls.Add(this.lbDakika);
+            this.Controls.Add(this.lbSaat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbSaat;
+        private System.Windows.Forms.Label lbDakika;
+        private System.Windows.Forms.Label lbSaniye;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ ZAMANLAYICI/ZAMANLAYICI/ZAMANLAYICI/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ZAMANLAYICI
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort serialPort;
+
+        #region Delegates
+        public delegate void UPDATE_SECOND_TEXT(string Second);
+        public delegate void UPDATE_MINUTE_TEXT(string Minute);
+        public delegate void UPDATE_HOUR_TEXT(string Hour);
+        #endregion
+
+        #region
+        public void UpdateHourText(string hour)
+        {
+            lbSaat.Text = hour;
+        }
+        public void UpdateMinuteText(string minute)
+        {
+            lbDakika.Text = minute;
+        }
+        public void UpdateSecondText(string second)
+        {
+            lbSaniye.Text = second;
+        }
+        #endregion
+
+        const byte HEADER = 0xAA;
+        private const int DATA_LENGTH = 3;
+        private byte[] rxBuffer = new byte[7];
+        private byte[] txBuffer = new byte[7];
+        #region
+        void SerialOnReciveHandler(object sender, SerialDataReceivedEventArgs e)
+
+        {
+            int bytesToRead = serialPort.BytesToRead;
+            byte[] receivedData = new byte[bytesToRead];
+            serialPort.Read(receivedData, 0, bytesToRead);
+
+            int rxindex = 0;
+
+            while (rxindex < receivedData.Length && receivedData[rxindex] != HEADER)
+            {
+                rxindex++;
+            }
+
+            if (rxindex >= receivedData.Length) return;
+
+            rxBuffer[0] = HEADER;
+
+            if (rxindex + 1 >= receivedData.Length) return;
+            rxBuffer[1] = receivedData[rxindex + 1];
+
+            if (rxindex + 2 >= receivedData.Length) return;
+            byte dataLength = receivedData[rxindex + 2];
+
+            if (dataLength != 3) return;
+
+            rxBuffer[2] = dataLength;
+
+            if (rxindex + 5 >= receivedData.Length) return;
+            rxBuffer[3] = receivedData[rxindex + 3];
+            rxBuffer[4] = receivedData[rxindex + 4];
+            rxBuffer[5] = receivedData[rxindex + 5];
+
+            if (rxindex + 6 >= receivedData.Length) return;
+            byte checksum = receivedData[rxindex + 6];
+
+            byte calculatedChecksum = (byte)(rxBuffer[1] + rxBuffer[2] + rxBuffer[3] + rxBuffer[4] + rxBuffer[5]);
+            if (checksum != calculatedChecksum) return;
+
+            rxBuffer[6] = checksum;
+
+            string hour = rxBuffer[5].ToString("D2");
+            if (lbSaat.InvokeRequired)
+            {
+                lbSaat.Invoke(new Action(() => lbSaat.Text = hour));
+            }
+            else
+            {
+                lbSaat.Text = hour;
+            }
+
+            string minute = rxBuffer[4].ToString("D2");
+            if (lbDakika.InvokeRequired)
+            {
+                lbDakika.Invoke(new Action(() => lbDakika.Text = minute));
+            }
+            else
+            {
+                lbSaat.Text = minute;
+            }
+
+            string second = rxBuffer[3].ToString("D2");
+            if (lbSaniye.InvokeRequired)
+            {
+                lbSaniye.Invoke(new Action(() => lbSaniye.Text = second));
+            }
+            else
+            {
+                lbSaniye.Text = second;
+            }
+
+        }
+        #endregion
+
+        private void InitializeSerialPort()
+        {
+            serialPort = new SerialPort
+            {
+                PortName = "COM16",
+                BaudRate = 115200,
+                Parity = Parity.None,
+                DataBits = 8,
+                StopBits = StopBits.One,
+                ReceivedBytesThreshold = 1,
+                Handshake = Handshake.None,
+                WriteTimeout = 3000,
+            };
+            try
+            {
+                serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Bağlantı hatası: " + ex.Message);
+            }
+            serialPort.DataReceived += SerialOnReciveHandler;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSerialPort();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+
+            if (serialPort.IsOpen)
+            {
+                serialPort.Close();
+            }
+            base.OnFormClosing(e);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                byte[] txBuffer = new byte[10];
+                byte[] txData = new byte[5];
+
+                byte txChecksum = 0;
+                byte txindex = 0;
+                byte txDataLength = 3;
+                byte commandStart = 0x01;
+
+                txBuffer[txindex++] = HEADER;
+                txBuffer[txindex++] = commandStart;
+                txBuffer[txindex++] = txDataLength;
+
+                for (int i = 0; i < txDataLength && i < txData.Length; i++)
+                {
+                    txBuffer[txindex++] = txData[i];
+                }
+
+                for (int i = 1; i < txindex; i++)
+                {
+                    txChecksum += txBuffer[i];
+                }
+
+                txBuffer[txindex++] = txChecksum;
+
+                serialPort.Write(txBuffer, 0, txindex);
+
+            }
+            else
+            {
+                MessageBox.Show("Port açık değil!");
+            }
+        }
+        
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (serialPort.IsOpen)
+            {
+                byte[] txBuffer = new byte[10];
+                byte[] txData = new byte[5];
+
+                byte txChecksum = 0;
+                byte txindex = 0;
+                byte txDataLength = 3;
+                byte commandStop = 0x02;
+
+                txBuffer[txindex++] = HEADER;
+                txBuffer[txindex++] = commandStop;
+                txBuffer[txindex++] = txDataLength;
+
+                for (int i = 0; i < txDataLength && i < txData.Length; i++)
+                {
+                    txBuffer[txindex++] = txData[i];
+                }
+
+                for (int i = 1; i < txindex; i++)
+                {
+                    txChecksum += txBuffer[i];
+                }
+
+                txBuffer[txindex++] = txChecksum;
+
+                serialPort.Write(txBuffer, 0, txindex);
+            }
+            else
+            {
+                MessageBox.Show("Port açık değil!");
+            }
+        }
+    }
+}
